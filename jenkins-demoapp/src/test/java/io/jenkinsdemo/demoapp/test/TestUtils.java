8b599a19541512e34660e0b4e8b9c8f88bc86a22
enstaneva <elitsastaneva@gmail.com>
2017-05-31 18:54:51 +0300

    Changing default host

--- jenkins-demoapp/src/test/java/io/jenkinsdemo/demoapp/test/TestUtils.java
+++ jenkins-demoapp/src/test/java/io/jenkinsdemo/demoapp/test/TestUtils.java
@@ -2,7 +2,7 @@ package io.jenkinsdemo.demoapp.test;
 
 public class TestUtils {
 
-	public static final String HOSTNAME = System.getProperty("app.host.name", "vmbgdck05");
+	public static final String HOSTNAME = System.getProperty("app.host.name", "localhost");
 	
 	public static final String APP_PORT = System.getProperty("app.port", "8090");
 }
